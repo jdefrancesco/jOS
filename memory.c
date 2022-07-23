7fb83516306723388091b89182dd1b787e9157bb
jdefr89 <jdefr89@gmail.com>
2022-07-23 16:45:08 -0400

    Working on MM

--- memory.c
+++ memory.c
@@ -1,11 +1,23 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #include "memory.h"
 #include "print.h"
 #include "debug.h"
 
+// Free a memory region.
+static void free_region(uint64_t v, uint64_t e);
+static PDPTR find_pml4t_entry(uint64_t map, uint64_t v, int alloc, uint32_t attribute);
+static PD find_pdpt_entry(uint64_t map, uint64_t v, int alloc, uint32_t attribute);
+static void setup_kvm(void);
+
+
 static struct free_mem_region_t free_mem_region[50];
+static struct page_t free_memory;
+static uint64_t memory_end;
+uint64_t page_map;
+extern char end;
 
 // We need to get the available memory from the BIOS
 void init_memory(void) 
@@ -22,7 +34,7 @@ void init_memory(void)
     // Make sure number of memory regions is less than fifty.
     ASSERT(count <= 50);
 
-    for (int i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++) {
         if (mem_map[i].type == 1) {
             free_mem_region[free_region_count].address = mem_map[i].address;
             free_mem_region[free_region_count].length = mem_map[i].length;
@@ -32,7 +44,77 @@ void init_memory(void)
 
         printk("%x  %uKB %u\n", mem_map[i].address, mem_map[i].length/1024, (uint64_t)mem_map[i].type);
     } 
+    
+    for (size_t i = 0; i < free_region_count; i++) {
+        // We need vstart and vend to use VA
+        uint64_t vstart = P2V(free_mem_region[i].address);
+        uint64_t vend = vstart + free_mem_region[i].length;
 
-    // Give total memory in MiB
-    printk("Total memory is %uKB (%uMB)\n", total_mem/1024, total_mem/1024/1024);
+        // We need address of end since its provided by linker script it's not a variable, it's treated as symbol.
+        if (vstart > (uint64_t)&end) {
+            free_region(vstart, vend); 
+        } else if (vend > (uint64_t)&end) {
+            free_region((uint64_t)&end, vend);
+        }
+    }
+
+    memory_end = (uint64_t) free_memory.next+PAGE_SIZE;
+    printk("%x\n", memory_end); 
+    
+}
+
+
+// free_region frees a previously allocated memory region.
+static void free_region(uint64_t v, uint64_t e) 
+{
+    for (size_t start = PA_UP(v); start+PAGE_SIZE <= e; start += PAGE_SIZE) {
+        if (v+PAGE_SIZE <= MAX_ADDR) {
+            kfree(start);
+        }
+    }
+}
+
+// Free memory adding it back to our pool.
+void kfree(uint64_t v) 
+{
+    ASSERT((v % PAGE_SIZE) == 0);
+    ASSERT(v >= (uint64_t)&end);
+    ASSERT(v+PAGE_SIZE <= MAX_ADDR);
+
+    struct page_t *page_addr = (struct page_t *)v;
+    page_addr->next = free_memory.next;
+    free_memory.next = page_addr;
+}
+
+// Allocate memory from our free memory region pool.
+void * kalloc(void)
+{
+    struct page_t *page_addr = free_memory.next;
+
+    if (page_addr != NULL) {
+        ASSERT(((uintptr_t)page_addr % PAGE_SIZE) == 0);
+        ASSERT((uintptr_t)page_addr >= (uint64_t)&end);
+        ASSERT(((uintptr_t)page_addr+PAGE_SIZE) <= MAX_ADDR);
+
+        // Remove page from list.
+        free_memory.next = page_addr->next;
+    } 
+
+    return (void *)page_addr;
+}
+
+static void setup_kvm(void)
+{
+    page_map = (uint64_t) kalloc();
+    ASSERT(page_map != 0);
+
+    memset((void*)page_map, 0x00, PAGE_SIZE);
+    bool status = map_pages(page_map, KERN_BASE, memory_end, V2P(KERN_BASE), PTE_P | PTE_W);
+}
+
+void init_kvm(void)
+{
+    setup_kvm();
+    switch_vm(page_map);
+    printk("Memory Manager initialized...");
 }
--- memory.h
+++ memory.h
@@ -16,6 +16,44 @@ struct free_mem_region_t {
     uint64_t length;
 };
 
+struct page_t {
+    struct page_t *next;
+};
+
+// Define our types for various page table levels.
+typedef uint64_t PDE;
+typedef PDE* PD;
+typedef PD* PDPTR;
+
+#define PTE_P 1
+#define PTE_W 2
+#define PTE_U 4
+#define PTE_ENTRY 0x80
+// Base address where our kernel lives (VA).
+#define KERN_BASE 0xffff800000000000
+// 2 MB Page size.
+#define PAGE_SIZE (2 * 1024 * 1024) 
+
+// The next to Macros convert from phys to virt addresses and vice-versa.
+// Need to take care of alignment which is why we shift things around.
+#define PA_UP(v) ((((uint64_t)(v) + PAGE_SIZE-1) >> 21 ) << 21)
+#define PA_DOWN(v) (((uint64_t)(v) >> 21) << 21)
+
+// Convert kernel virtual address Macros.
+#define P2V(p) ((uint64_t) (p) + KERN_BASE)
+#define V2P(v) ((uint64_t) (v) - KERN_BASE)
+
+// Kernel has 1GB VMA. This represents the end of that address space.
+#define MAX_ADDR 0xffff800040000000
+
+/*
+ * Function Prototypes..
+ */
 void init_memory(void);
+void * kalloc(void);
+void kfree(uint64_t v);
+bool map_pages(uint64_t map, uint64_t v, uint64_t e, uint64_t pa, uint32_t attribute);
+void switch_vm(uint64_t map);
+void init_kvm(void);
 
 #endif 
